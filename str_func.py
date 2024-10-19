7e35f611988124b116cda7c0aa2923724b6d084f
Valentin Altabasov <Valenjir@gmail.com>
2024-10-19 14:55:06 +0300

    add str_func add more str func

--- str_func.py
+++ str_func.py
@@ -1,4 +1,6 @@
 def upper_word(str):
     return str.upper()
 
-
+"""функция, которая делает заглавными первые буквы каждого слова в строке"""
+def word(str):
+    return ' '.join(word.capitalize() for word in str.split())
